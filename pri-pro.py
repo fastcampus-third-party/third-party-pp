a41fcf3ca945782ca3a282a98b59a02cb5f5f4ef
moongs95 <juliaanswls@naver.com>
2025-08-22 18:41:05 +0900

    feat: Create pri-pro.py

---
+++ pri-pro.py
@@ -0,0 +1,7 @@
+import random
+
+if __name__ == '__main__':
+    n = 100000
+    print(" Simulation count:", n)
+    print(f" Random play wins: {play_random(n):4.1f}% of simulations")
+    print(f"Optimal play wins: {play_optimal(n):4.1f}% of simulations")
