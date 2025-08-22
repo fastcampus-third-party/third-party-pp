11a7cad6c25f7e1813fdc82a335b2978f84b40dd
yekyung821 <bubblekid43@gmail.com>
2025-08-22 18:56:07 +0900

    feat: implement play_optimal function

--- pri-pro-yk.py
+++ pri-pro-yk.py
@@ -1,3 +1,5 @@
+import random
+
 def play_optimal(n):
     # using 0-99 instead of ranges 1-100
     pardoned = 0
@@ -18,3 +20,9 @@ def play_optimal(n):
         if found:
             pardoned += 1
     return pardoned / n * 100   # %
+
+if __name__ == '__main__':
+    n = 100000
+    print(" Simulation count:", n)
+    print(f" Random play wins: {play_random(n):4.1f}% of simulations")
+    print(f"Optimal play wins: {play_optimal(n):4.1f}% of simulations")
